3f652167f4c5f9e001a3c6a533da51f128b683f4
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-12 15:54:42 +0200

    Rework: changed functions position

--- Ex-EvenorOdd/js/app.js
+++ Ex-EvenorOdd/js/app.js
@@ -1,6 +1,23 @@
 // #Numero pari o dispari
-// Chiedo all'utente pari o dispari ed un numero da 1 a 5
 
+// Functions
+// Funzione per un numero random tra 1 e 5
+function getRandomNumber (min,max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min );
+}
+
+// Funzione per sommare i numeri del pc e dell'utente e definire se sia pari o dispari
+function numberSumOddEven (num1,num2) {
+    sum = num1 + num2;
+    console.log(sum);
+    const even = sum % 2 === 0;
+    return even;
+}
+console.log(numberSumOddEven(userNumber,pcNumber));
+
+// Chiedo all'utente pari o dispari ed un numero da 1 a 5
 const userOddEvenChose = prompt('Scegli tra Pari o Dispari').trim().toLowerCase();
 console.log(userOddEvenChose, typeof(userOddEvenChose));
 
@@ -19,21 +36,3 @@ console.log(pcNumber);
     } else {
         console.log('Hai perso, riprova');
     }
-
-// Functions
-// Funzione per un numero random tra 1 e 5
-function getRandomNumber (min,max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min );
-}
-
-// Funzione per sommare i numeri del pc e dell'utente e definire se sia pari o dispari
-function numberSumOddEven (num1,num2) {
-    sum = num1 + num2;
-    console.log(sum);
-    const even = sum % 2 === 0;
-    return even;
-}
-
-console.log(numberSumOddEven(userNumber,pcNumber));
