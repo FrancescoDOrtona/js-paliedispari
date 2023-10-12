77a4e366cd2639dd645fe7d64cee55fc3e4dc703
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-12 14:25:58 +0200

    Add: function for invert the word

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 // #Parole Palindrome
 // Chiedo all'utente di inserire una parola
-const word = prompt("Inserisci una parola e ti dirò se è palindroma")
-console.log(word,typeof(word))
+const word = prompt("Inserisci una parola e ti dirò se è palindroma");
+console.log(word,typeof(word));
+
+
+
+// Functions
+// creo una funzione che inverte la parola inserita
+function invertWord (string){
+    const inverseString = string.split('').reverse().join('');
+    return inverseString;
+}
+
+console.log(invertWord(word));
