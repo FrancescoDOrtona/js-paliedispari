f3a688c8df73f8adc962691f719edfa5e3aa80bf
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-12 15:05:55 +0200

    Add: function for sum & even number

--- Ex-EvenorOdd/js/app.js
+++ Ex-EvenorOdd/js/app.js
@@ -7,14 +7,23 @@ console.log(userOddEvenChose, typeof(userOddEvenChose));
 const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
 console.log(userNumber);
 
-const pcNumber = getRandomNumber(1,5)
-
+const pcNumber = getRandomNumber(1,5);
+console.log(pcNumber);
 
 // Functions
+// Funzione per un numero random tra 1 e 5
 function getRandomNumber (min,max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min );
 }
 
-console.log(pcNumber)
+// Funzione per sommare i numeri del pc e dell'utente e definire se sia pari o dispari
+function numberSumOddEven (num1,num2) {
+    sum = num1 + num2;
+    console.log(sum);
+    const even = sum % 2 === 0;
+    return even;
+}
+
+console.log(numberSumOddEven(userNumber,pcNumber));
