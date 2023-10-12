b5b60d17734e6488e5fd5dd24c178e383e1377a4
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-12 14:52:39 +0200

    Add: JS user choise

---
+++ Ex-EvenorOdd/js/app.js
@@ -0,0 +1,8 @@
+// #Numero pari o dispari
+// Chiedo all'utente pari o dispari ed un numero da 1 a 5
+
+const userOddEvenChose = prompt('Scegli tra Pari o Dispari')
+console.log(userOddEvenChose, typeof(userOddEvenChose))
+
+const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'))
+console.log(userNumber)
